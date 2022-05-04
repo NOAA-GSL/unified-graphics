0809bc923b27054a2dc0f02ce53bbd530a68db8f
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-05-04 14:41:18 -0600

    Add USWDS
    I added USWDS, Sass, and svelte-preprocess to the project with a basic
    stylesheet that pulls in all of USWDS (for now). We'll probably want to
    try pairing down the things we import, once this is working.
    
    I can't figure out how to configure additional include paths for Sass.
    There's nothing I can find in the svelte-preprocess docs that mention
    it, and my guess based on how I've done it in the past with Svelte,
    Rollup, and svelte-preprocess-sass have not worked.

--- services/ui/svelte.config.js
+++ services/ui/svelte.config.js
@@ -1,10 +1,21 @@
 import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
+import { sass } from 'sass';
+
+const USWDS_PACKAGES = new URL('./node_modules/@uswds/uswds/packages', import.meta.url).toString();
+
+console.log(USWDS_PACKAGES);
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	}
+  preprocess: preprocess({
+    implementation: sass({
+      includePaths: [USWDS_PACKAGES]
+    })
+  }),
+  kit: {
+    adapter: adapter()
+  }
 };
 
 export default config;
