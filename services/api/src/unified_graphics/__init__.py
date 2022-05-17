4a8258c1742bf3514cb3b89af76ab19051466492
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-05-17 08:51:31 -0600

    Create a skeleton flask app
    The app has a single route that just returns a hard-coded JSON object
    for testing. This will enable us to determine whether we have the
    service running or not. It has a rough structure that the Flask app will
    use—including logging, the [app factory pattern][1], and a WSGI module
    for use with Gunicorn.
    
    The routes module may get refactored, depending on how complex the app
    becomes. We may adopt a more Django-like structure with purpose-specific
    modules (e.g. diag/routes.py, forecasts/routes.py, etc.).
    
    [1]: https://flask.palletsprojects.com/en/2.1.x/patterns/appfactories/

--- services/api/src/unified_graphics/__init__.py
+++ services/api/src/unified_graphics/__init__.py
@@ -1,1 +1,38 @@
+from logging.config import dictConfig
+
+from flask import Flask
+
+
 __version__ = "0.1.0"
+
+
+def create_app(config=None):
+    dictConfig(
+        {
+            "version": 1,
+            "formatters": {
+                "default": {
+                    "format": "[%(asctime)s] %(levelname)s in %(module)s: %(message)s",
+                },
+            },
+            "handlers": {
+                "wsgi": {
+                    "class": "logging.StreamHandler",
+                    "stream": "ext://flask.logging.wsgi_errors_stream",
+                    "formatter": "default",
+                }
+            },
+            "root": {
+                "level": "INFO",
+                "handlers": ["wsgi"],
+            },
+        }
+    )
+
+    app = Flask(__name__)
+
+    from . import routes
+
+    app.register_blueprint(routes.bp)
+
+    return app
