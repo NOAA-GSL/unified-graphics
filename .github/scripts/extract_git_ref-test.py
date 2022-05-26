fbfc8b724bcfe4e6a5927018572cf831481d72a1
Ian McGinnis <ian.mcginnis@noaa.gov>
2022-05-26 14:39:39 -0600

    Refactor GHA branch name extraction script
    - Make use of guard clauses in extract_git_ref.py to improve readability.
    - Move env processing into main() to make testing easier.
    - Add tests for extract_get_ref.py to make sure we're doing things
    correctly.
    - Add a CI job to lint and test the scripts in here so we meet our code
    standards and make sure changes are valid.

---
+++ .github/scripts/extract_git_ref-test.py
@@ -0,0 +1,58 @@
+import pytest
+
+from extract_git_ref import get_branch
+
+
+def test_push_heads():
+    branch = get_branch(gh_event="push", gh_ref="refs/heads/foo", gh_head_ref=None)
+
+    assert branch == "BRANCH=foo"
+
+
+def test_push_tags():
+    branch = get_branch(gh_event="push", gh_ref="refs/tags/1.2.3", gh_head_ref=None)
+
+    assert branch == "BRANCH=1.2.3"
+
+
+def test_push_bad_ref():
+    with pytest.raises(
+        ValueError,
+        match="Unanticipated git ref from a GitHub push event: refs/baz/1.2.3",
+    ):
+        get_branch(gh_event="push", gh_ref="refs/baz/1.2.3", gh_head_ref=None)
+
+
+def test_pr_heads():
+    branch = get_branch(
+        gh_event="pull_request", gh_ref="refs/heads/baz", gh_head_ref="baz"
+    )
+
+    assert branch == "BRANCH=baz"
+
+
+def test_pr_unset_head_ref():
+    with pytest.raises(ValueError, match="GITHUB_HEAD_REF isn't set"):
+        get_branch(gh_event="pull_request", gh_ref="refs/heads/baz", gh_head_ref=None)
+
+
+def test_workflow_dispatch_heads():
+    branch = get_branch(
+        gh_event="workflow_dispatch", gh_ref="refs/heads/bar/baz", gh_head_ref=None
+    )
+
+    assert branch == "BRANCH=bar/baz"
+
+
+def test_workflow_dispatch_tags():
+    with pytest.raises(
+        ValueError, match="Unanticipated git ref from a GitHub workflow_dispatch event"
+    ):
+        get_branch(
+            gh_event="workflow_dispatch", gh_ref="refs/tags/x.y.z", gh_head_ref=None
+        )
+
+
+def test_bad_gh_event():
+    with pytest.raises(ValueError, match="Unanticipated GitHub Event: foo"):
+        get_branch(gh_event="foo", gh_ref="refs/tags/x.y.z", gh_head_ref=None)
--- .github/scripts/extract_git_ref.py
+++ .github/scripts/extract_git_ref.py
@@ -1,42 +1,38 @@
 #!/usr/bin/env python3
 
+from typing import Optional
 import os
+import re
 
-def get_branch():
+
+def get_branch(gh_event: str, gh_ref: str, gh_head_ref: Optional[str]) -> str:
+    """Process GitHub Actions env variables to extract the branch or tag name"""
+
+    if gh_event not in ["pull_request", "push", "workflow_dispatch"]:
+        raise ValueError(f"Unanticipated GitHub Event: {gh_event}")
+
+    if gh_event == "pull_request":
+        if gh_head_ref is None:
+            raise ValueError("GITHUB_HEAD_REF isn't set")
+        return f"BRANCH={gh_head_ref}"
+
+    # Extract the branch name from the current git ref
+    ref_match = re.match(r"refs/(heads|tags)/(.+)$", gh_ref)
+
+    if not ref_match or (gh_event == "workflow_dispatch" and ref_match[1] != "heads"):
+        raise ValueError(
+            f"Unanticipated git ref from a GitHub {gh_event} event: {gh_ref}"
+        )
+
+    return f"BRANCH={ref_match[2]}"
+
+
+if __name__ == "__main__":
     gh_event = os.environ["GITHUB_EVENT_NAME"]
     gh_ref = os.environ["GITHUB_REF"]
     gh_head_ref = os.environ.get("GITHUB_HEAD_REF")  # only exists in PRs
+    branch = get_branch(gh_event, gh_ref, gh_head_ref)
 
-    if gh_event == "pull_request":
-        if gh_head_ref is not None:
-            msg = f"BRANCH={gh_head_ref}"
-        else:
-            raise ValueError("GITHUB_HEAD_REF isn't set")
-    elif gh_event == "push":
-        # Handle differences between branches/tags
-        branch = "refs/heads/"
-        tag = "refs/tags/"
-        if gh_ref.startswith(branch):
-            msg = f"BRANCH={gh_ref[len(branch):]}"
-        elif gh_ref.startswith(tag):
-            msg = f"BRANCH={gh_ref[len(tag):]}"
-        else:
-            raise ValueError("Unanticipated git ref from a GitHub push event")
-    elif gh_event == "workflow_dispatch":
-        # Workflow Dispatch should only be able to trigger from branches
-        branch = "refs/heads/"
-        if gh_ref.startswith(branch):
-            msg = f"BRANCH={gh_ref[len(branch):]}"
-        else:
-            raise ValueError("Unanticipated git ref from a GitHub workflow_dispatch event")
-    else:
-        raise ValueError("Unanticipated GitHub Event")
-    return msg
-
-
-if __name__ == '__main__':
     gh_env_file = os.environ["GITHUB_ENV"]
-    branch = get_branch()
-
     with open(gh_env_file, "a") as env_file:
         env_file.write(branch + "\n")
