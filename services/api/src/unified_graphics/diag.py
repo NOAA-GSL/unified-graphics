ea8c99139cace0d50e1e610f109b0a7a3149506f
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-06-14 08:36:25 -0600

    Fix type annotations for Python 3.8
    Unfortunately, our tests are running in Python 3.8 instead of 3.9, so we
    can't use tuple[...], we have to use typing.Tuple[...].

--- services/api/src/unified_graphics/diag.py
+++ services/api/src/unified_graphics/diag.py
@@ -1,8 +1,11 @@
+from typing import Tuple
 import xarray as xr
 
 
-def get_diagnostics() -> tuple[xr.Dataset, xr.Dataset]:
-    guess: xr.Dataset = xr.open_dataset("data/ncdiag_conv_t_ges.nc4.20220514")
-    analysis: xr.Dataset = xr.open_dataset("data/ncdiag_conv_t_anl.nc4.20220514")
+# FIXME: typing.Tuple is deprecated in Python 3.9. Once we are off 3.8 in CI, we
+# should replace this tuple[xr.Dataset, xr.Dataset]
+def get_diagnostics() -> Tuple[xr.Dataset, xr.Dataset]:
+    guess = xr.open_dataset("data/ncdiag_conv_t_ges.nc4.20220514")
+    analysis = xr.open_dataset("data/ncdiag_conv_t_anl.nc4.20220514")
 
     return (guess, analysis)
