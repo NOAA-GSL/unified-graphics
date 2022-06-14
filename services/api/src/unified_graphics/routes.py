f8b2231c0bb5581db3d8f92e67efb39e0595b59c
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-06-14 14:06:55 -0600

    Update diag route for new get_diagnostics
    The route will make two calls to diag.get_diagnostics in order to get
    the statistics for the guess loop and the analysis loop, since
    get_diagnostics now returns only a single dictionary instead of a tuple
    of Datasets. We're back to all tests passing now.

--- services/api/src/unified_graphics/routes.py
+++ services/api/src/unified_graphics/routes.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, current_app, jsonify
-import numpy as np
 
 from unified_graphics import diag
 
@@ -15,27 +14,6 @@ def index():
 
 @bp.route("/diag/temperature/")
 def diag_temperature():
-    def histogram(data):
-        counts, bins = np.histogram(data, bins="auto")
-        for idx, count in enumerate(counts):
-            yield {"lower": bins[idx], "upper": bins[idx + 1], "value": int(count)}
-
-    current_app.logger.info("diag_temperature()")
-
-    guess, analysis = diag.get_diagnostics()
-
-    guess_diag = {
-        "bins": list(histogram(guess["Obs_Minus_Forecast_adjusted"].values)),
-        "observations": len(guess["Obs_Minus_Forecast_adjusted"]),
-        "std": np.std(guess["Obs_Minus_Forecast_adjusted"].values),
-        "mean": np.mean(guess["Obs_Minus_Forecast_adjusted"].values),
-    }
-
-    analysis_diag = {
-        "bins": list(histogram(analysis["Obs_Minus_Forecast_adjusted"].values)),
-        "observations": len(analysis["Obs_Minus_Forecast_adjusted"]),
-        "std": np.std(analysis["Obs_Minus_Forecast_adjusted"].values),
-        "mean": np.mean(analysis["Obs_Minus_Forecast_adjusted"].values),
-    }
-
-    return jsonify(guess=guess_diag, analysis=analysis_diag)
+    guess = diag.get_diagnostics()
+    analysis = diag.get_diagnostics()
+    return jsonify(guess=guess, analysis=analysis)
