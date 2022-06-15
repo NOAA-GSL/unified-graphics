0c62751e7b8b71bd8b8e7872b6ac59eb5b278eec
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-06-15 07:58:42 -0600

    Test that the temperature diag route opens both files
    I've returned to using unittest.mock instead of monkeypatch because
    unittest.mock also acts as a function spy, so I can assert that
    open_dataset is being called with the correct file paths. Currently, the
    functions are hard-coded to load a single file, so the tests now fail
    (good).

--- services/api/tests/test_unified_graphics.py
+++ services/api/tests/test_unified_graphics.py
@@ -1,3 +1,5 @@
+from unittest.mock import call, patch
+
 import xarray as xr
 
 
@@ -7,13 +9,26 @@ def test_root_endpoint(client):
     assert response.json == {"msg": "Hello, Dave"}
 
 
-def test_temperature_diag_distribution(monkeypatch, client):
-    def mock_open_dataset(*args, **kwargs):
-        return xr.Dataset({"Obs_Minus_Forecast_adjusted": [-1, 1, 1, 2, 3]})
+def test_temperature_diag_distribution(client):
+    with patch("xarray.open_dataset") as mock_open_dataset:
+        mock_open_dataset.return_value = xr.Dataset(
+            {"Obs_Minus_Forecast_adjusted": [-1, 1, 1, 2, 3]}
+        )
 
-    monkeypatch.setattr(xr, "open_dataset", mock_open_dataset)
+        response = client.get("/diag/temperature/")
 
-    response = client.get("/diag/temperature/")
+    # FIXME: I don't love asserting these calls to xarray.open_dataset to ensure
+    # that the route is looking up both the guess and the analysis diagnostics,
+    # it seems like an implementation detail. The alternative seems to be to
+    # implement a more complex mock that returns different arrays based on
+    # filenames.
+    mock_open_dataset.assert_has_calls(
+        [
+            call("/test/data/ncdiag_conv_t_ges.nc4.20220514"),
+            call("/test/data/ncdiag_conv_t_anl.nc4.20220514"),
+        ],
+        any_order=True,
+    )
 
     assert response.json == {
         "guess": {
