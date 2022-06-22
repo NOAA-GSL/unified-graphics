fc2d61cc469e88afd3718d8c02c7dbeddd3fcd9a
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-06-22 13:34:14 -0600

    Add vitest and testing-library/svelte for unit testing
    Separate out integration tests and unit tests for components. Vitest and
    @testing-library/svelte were the recommendations I found on the Svelte
    discord for this. I tried Playwright's experimental component testing
    feature, but I couldn't get it going in this project, so we'll limit our
    use of Playwright to integration tests right now. Maybe we can revisit
    the component testing in Playwright later.

---
+++ services/ui/tests/unit/header.spec.js
@@ -0,0 +1,14 @@
+import { cleanup, render } from "@testing-library/svelte";
+import { afterEach, describe, it, expect } from "vitest";
+
+import Header from "../../src/lib/Header/Header.svelte";
+
+describe("Header.svelte", () => {
+  afterEach(() => cleanup());
+
+  it("has expected text", async () => {
+    const result = render(Header);
+
+    expect(result.getByRole("banner").innerHTML).toContain("Unified Graphics");
+  });
+});
