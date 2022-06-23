3874c05a8b4ffac834a80f079b20fa187fb10381
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-06-23 14:06:49 -0600

    Add an x-axis to the histogram
    I imagine we'll need to refactor this stuff out of here and into
    something reusable, but for now I'm just hard-coding it into the
    histogram until it becomes clear how else we want to use our axes.

--- services/ui/src/lib/components/VariableDiag/Histogram.svelte
+++ services/ui/src/lib/components/VariableDiag/Histogram.svelte
@@ -1,25 +1,72 @@
 <script>
-  import { scaleLinear, min, max, extent } from "d3";
+  import { format, scaleLinear, min, max, extent } from "d3";
 
   export let width = 0;
   export let height = 0;
 
   export let bins = [];
 
+  let margin = {
+    top: 0,
+    right: 0,
+    bottom: 32,
+    left: 0,
+  };
+
+  let formatNum = format("~s");
+
   $: domain = [min(bins, (d) => d.lower), max(bins, (d) => d.upper)];
   $: range = extent(bins, (d) => d.value);
 
-  $: x = scaleLinear(domain, [0, width]);
-  $: y = scaleLinear(range, [height, 0]);
+  $: x = scaleLinear(domain, [margin.left, width - margin.left - margin.right]);
+  $: y = scaleLinear(range, [height - margin.top - margin.bottom, margin.bottom]);
 </script>
 
 <svg viewBox="0 0 {width} {height}">
   {#each bins as bin}
     <rect
+      class="bar"
       x={x(bin.lower)}
       y={y(bin.value)}
       width={x(bin.upper) - x(bin.lower)}
       height={y(0) - y(bin.value)}
     />
   {/each}
+  <g class="x-axis" transform="translate({x(domain[0])}, {y(range[0])})">
+    <path class="domain" d="M 1,8 l 0,-8 {x(domain[1]) - 2},0 0,8" />
+    {#each x.ticks() as tick}
+      <g class="tick" transform="translate({x(tick)}, 0)">
+        <line y2="8" />
+        <text y="10">{formatNum(tick)}</text>
+      </g>
+    {/each}
+  </g>
 </svg>
+
+<style lang="scss">
+  @use "uswds-core" as uswds;
+
+  .domain {
+    stroke-width: 2px;
+    stroke: uswds.color("base-dark");
+    fill: none;
+  }
+
+  .bar {
+    fill: uswds.color("accent-cool-darker");
+  }
+
+  .tick {
+    line {
+      stroke: uswds.color("base-dark");
+    }
+
+    text {
+      font-family: inherit;
+      font-size: uswds.size("ui", "3xs");
+
+      dominant-baseline: hanging;
+      text-anchor: middle;
+    }
+  }
+</style>
