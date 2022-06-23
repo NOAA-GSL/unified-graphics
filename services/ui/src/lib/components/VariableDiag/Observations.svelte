d1bf8ba8c55d35525bc8e277daed361113442626
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-06-23 08:53:45 -0600

    Format the diag statistics

--- services/ui/src/lib/components/VariableDiag/Observations.svelte
+++ services/ui/src/lib/components/VariableDiag/Observations.svelte
@@ -1,4 +1,6 @@
 <script>
+  import { format } from "d3";
+
   import Histogram from "./Histogram.svelte";
 
   export let title;
@@ -6,23 +8,37 @@
   export let mean;
   export let std;
   export let bins;
+
+  const formatNum = format(",");
+  const formatDecimal = format(",.3f");
 </script>
 
 <div>
   <h3>{title}</h3>
   <dl>
-    <dt>Observations:</dt>
-    <dd>{observations}</dd>
-    <dt>Mean:</dt>
-    <dd>{mean}</dd>
-    <dt>Std. Dev.:</dt>
-    <dd>{std}</dd>
+    <div>
+      <dt>Observations:</dt>
+      <dd>{formatNum(observations)}</dd>
+    </div>
+    <div>
+      <dt>Mean:</dt>
+      <dd>{formatDecimal(mean)}</dd>
+    </div>
+    <div>
+      <dt>Std. Dev.:</dt>
+      <dd>{formatDecimal(std)}</dd>
+    </div>
   </dl>
   <Histogram {bins} />
 </div>
 
 <style lang="scss">
-  div {
-    aspect-ratio: 1/1;
+  dt,
+  dd {
+    display: inline-block;
+  }
+
+  dt {
+    font-weight: bold;
   }
 </style>
