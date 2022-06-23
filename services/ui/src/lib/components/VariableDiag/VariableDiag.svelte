4ea055295c2c0594778335739a942033a4a0c9cf
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-06-23 14:21:05 -0600

    Add a little space below the variable name

--- services/ui/src/lib/components/VariableDiag/VariableDiag.svelte
+++ services/ui/src/lib/components/VariableDiag/VariableDiag.svelte
@@ -6,10 +6,18 @@
   export let analysis;
 </script>
 
-<article>
+<article class="flow">
   <h2>{title}</h2>
   <div data-layout="cluster">
     <Observations title="Observation - Background" {...guess} />
     <Observations title="Observation - Analysis" {...analysis} />
   </div>
 </article>
+
+<style lang="scss">
+  @use "uswds-core" as uswds;
+
+  article {
+    --space: #{uswds.units(1)};
+  }
+</style>
