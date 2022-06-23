040cf83ff25a2d327ebee08aef369e50d085e5fc
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-06-23 13:33:35 -0600

    Apply a grid layout to the main application
    Using the grid layout from Every Layout for the main element in the
    application. Tested by duplicating the Temperature diag card.

--- services/ui/src/routes/__layout.svelte
+++ services/ui/src/routes/__layout.svelte
@@ -9,10 +9,19 @@
 </main>
 
 <style lang="scss">
+  @use "uswds-core" as uswds;
+
   main {
     flex-grow: 1;
-    place-items: center;
+    grid-template-columns: repeat(
+      auto-fit,
+      minmax(min(uswds.units("mobile"), 100%), 1fr)
+    );
+    gap: uswds.units(2);
+    align-items: start;
 
     display: grid;
+
+    padding: uswds.units(2);
   }
 </style>
