f7301288a736448e4834546e54f2444dbfb6f7b1
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-06-30 08:01:10 -0600

    Remove the __DIAG_API_HOST__ token

--- services/ui/src/lib/helpers/api.helper.js
+++ services/ui/src/lib/helpers/api.helper.js
@@ -1,10 +1,5 @@
-// The string `__DIAG_API_HOST__` gets replaced by Vite with the value of
-// process.env.UG_DIAG_API_HOST so that we can change where the API is expected
-// to be
-const DIAG_API_HOST = "__DIAG_API_HOST__";
-
 export function diagnostics() {
-  return fetch(`${DIAG_API_HOST}/diag/temperature`).then((response) => response.json());
+  return fetch("/diag/temperature").then((response) => response.json());
 }
 
 export default {
