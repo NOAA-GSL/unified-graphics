7bd1c6ab87e7c917a8d2cb556baa5ab1c8e5cbe6
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-07-05 13:30:35 -0600

    Add logging to the temperature diag route

--- services/ui/src/routes/diag/temperature.js
+++ services/ui/src/routes/diag/temperature.js
@@ -1,7 +1,19 @@
+import debug from "debug";
+
 import env from "$lib/helpers/env.helpers";
 
+const logger = debug("unified-graphics:route");
+
 export async function get() {
-  const response = await fetch(`${env.diagApiHost}/diag/temperature/`);
+  logger("GET /diag/temperature/");
+
+  const endpoint = `${env.diagApiHost}/diag/temperature/`;
+
+  logger("Fetching %s", endpoint);
+
+  const response = await fetch(endpoint);
+
+  logger("Received: %s bytes", response.headers.get("content-length"));
 
   return response;
 }
