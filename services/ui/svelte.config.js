0f32fd0f7f6f13af37a76becefc684817defcb8f
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-07-08 07:31:31 -0600

    Update Vite config and SK boilerplate
    Caught an update from SvelteKit that made some changes to how Vite is
    configured and the strings that get replaced in the boilerplate HTML, so
    I had to resolve those to get the site building and tests passing.

--- services/ui/svelte.config.js
+++ services/ui/svelte.config.js
@@ -1,8 +1,7 @@
 import adapter from "@sveltejs/adapter-node";
 import preprocess from "svelte-preprocess";
 
-import { fileURLToPath } from "node:url";
-
+// FIXME: Duplicated from vite.config.js
 const scss = {
   includePaths: ["node_modules/@uswds/uswds/packages"],
 };
@@ -14,19 +13,6 @@ const config = {
     adapter: adapter(),
     prerender: {
       default: true,
-    },
-    vite: {
-      css: {
-        preprocessorOptions: { scss },
-      },
-      resolve: {
-        alias: {
-          "/fonts": fileURLToPath(
-            new URL("./node_modules/@uswds/uswds/dist/fonts", import.meta.url)
-          ),
-          styles: fileURLToPath(new URL("./src/styles/_index.scss", import.meta.url)),
-        },
-      },
     },
   },
 };
