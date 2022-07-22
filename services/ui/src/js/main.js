35e0bbc3960f13b51276a7bc3f70d775140506e7
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-07-21 21:50:07 -0600

    Rebuild the header in index.html
    Move the SvelteKit app.html to index.html for Vite and recreate the
    header from the Header.svelte component. Create a main.js file to import
    the Sass styles so Vite will compile the styles, and add a few more
    utilities from USWDS.

---
+++ services/ui/src/js/main.js
@@ -0,0 +1,1 @@
+import styles from "../styles/_index.scss"; // eslint-disable-line
--- services/ui/vite.config.js
+++ services/ui/vite.config.js
@@ -5,6 +5,7 @@ const scss = {
 };
 
 const config = {
+  root: "src/",
   css: {
     preprocessorOptions: { scss },
   },
