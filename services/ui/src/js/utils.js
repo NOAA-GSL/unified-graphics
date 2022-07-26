202af4ff0ecb1ae3a90065fc54554a9d5ecc3759
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-07-26 08:41:40 -0600

    Add a zip utility
    Add a utility for zipping multiple arrays together just like Python's
    zip() built-in.

---
+++ services/ui/src/js/utils.js
@@ -0,0 +1,9 @@
+export function* zip(...args) {
+  if (args.length < 1) return [];
+
+  const n = args.map((arr) => arr.length).reduce((a, b) => Math.min(a, b));
+
+  for (let i = 0; i < n; i++) {
+    yield args.map((arr) => arr[i]);
+  }
+}
