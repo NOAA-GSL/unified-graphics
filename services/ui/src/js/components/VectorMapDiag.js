260d3760b946c4aca6ebc24d83e62f0ce66b110e
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-07-26 15:06:31 -0600

    Replace the SVG with a canvas for the map
    If we're plotting all of the observations, an SVG is not performant;
    there are too many DOM elements. So we'll use a canvas instead.

--- services/ui/src/js/components/VectorMapDiag.js
+++ services/ui/src/js/components/VectorMapDiag.js
@@ -1,16 +1,20 @@
-import { extent } from "d3-array";
-import { scaleDiverging, scaleLinear } from "d3-scale";
-import { interpolateRdBu } from "d3-scale-chromatic";
-import { select } from "d3-selection";
-
-import { zip } from "../utils";
-
 export default class VectorMapDiag extends HTMLElement {
   static #TEMPLATE = `<slot name=title></slot>
-  <svg></svg>`;
+  <canvas></canvas>`;
 
   static #STYLE = `<style>
-    svg {
+    :host {
+      display: flex;
+      flex-direction: column;
+    }
+
+    * {
+      flex: 0 0 auto;
+      margin: 0;
+    }
+
+    canvas {
+      flex: 1 1 auto;
       aspect-ratio: 4 / 3;
     }
   </style>`;
@@ -50,79 +54,23 @@ export default class VectorMapDiag extends HTMLElement {
     this.resizeObserver = new ResizeObserver(() => {
       this.update();
     });
-    this.resizeObserver.observe(this.shadowRoot?.querySelector("svg"));
+    this.resizeObserver.observe(this.shadowRoot?.querySelector("canvas"));
     this.update();
   }
 
   disconnectedCallback() {
-    this.resizeObserver?.unobserve(this.shadowRoot?.querySelector("svg"));
+    this.resizeObserver?.unobserve(this.shadowRoot?.querySelector("canvas"));
     delete this.resizeObserver;
   }
 
   update() {
-    const svg = select(this.shadowRoot).select("svg");
-    const { height, width } = svg.node().getBoundingClientRect();
+    const canvas = this.shadowRoot?.querySelector("canvas");
 
-    svg.attr("viewBox", `0 0 ${width} ${height}`);
+    if (!canvas) return;
 
-    const loop = this.#data.guess;
+    const { height, width } = canvas.getBoundingClientRect();
 
-    if (!loop) return;
-
-    const speed = [];
-    for (const [obs, fcst, coord] of zip(
-      loop.observation.magnitude,
-      loop.forecast.magnitude,
-      loop.coords
-    )) {
-      speed.push([...coord, obs - fcst]);
-    }
-
-    const x = scaleLinear(
-      extent(speed, (d) => d[0]),
-      [0, width]
-    );
-    const y = scaleLinear(
-      extent(speed, (d) => d[1]),
-      [height, 0]
-    );
-
-    const [minVal, maxVal] = extent(speed, (d) => d[2]);
-    const fill = scaleDiverging([minVal, 0, maxVal], interpolateRdBu);
-
-    svg
-      .selectAll("circle")
-      .data(speed)
-      .join("circle")
-      .attr("r", 4)
-      .attr("cx", (d) => x(d[0]))
-      .attr("cy", (d) => y(d[1]))
-      .attr("fill", (d) => fill(d[2]));
-
-    const obs = Array.from(zip(loop.observation.direction, loop.coords));
-
-    svg
-      .selectAll(".observation")
-      .data(obs)
-      .join("line")
-      .attr("stroke", "black")
-      .attr("stroke-width", "1px")
-      .attr("x1", (d) => x(d[1][0]))
-      .attr("y1", (d) => y(d[1][1]))
-      .attr("x2", (d) => x(d[1][0]) + 12 * Math.cos(((d[0] + 90) * Math.PI) / 180))
-      .attr("y2", (d) => y(d[1][1]) + 12 * Math.sin(((d[0] + 90) * Math.PI) / 180));
-
-    const fcst = Array.from(zip(loop.forecast.direction, loop.coords));
-
-    svg
-      .selectAll(".forecast")
-      .data(fcst)
-      .join("line")
-      .attr("stroke", "black")
-      .attr("stroke-width", "1px")
-      .attr("x1", (d) => x(d[1][0]))
-      .attr("y1", (d) => y(d[1][1]))
-      .attr("x2", (d) => x(d[1][0]) + 6 * Math.cos(((d[0] + 90) * Math.PI) / 180))
-      .attr("y2", (d) => y(d[1][1]) + 6 * Math.sin(((d[0] + 90) * Math.PI) / 180));
+    canvas.setAttribute("width", width.toString());
+    canvas.setAttribute("height", height.toString());
   }
 }
