da7390f1d8a1dd846d0211ca429b4f5a103b0436
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-07-26 10:59:35 -0600

    Visualize differences in wind direction
    Add some lines to the SVG to represent the wind directions from the
    observations and the forecast value.

--- services/ui/src/js/components/VectorMapDiag.js
+++ services/ui/src/js/components/VectorMapDiag.js
@@ -69,34 +69,60 @@ export default class VectorMapDiag extends HTMLElement {
 
     if (!loop) return;
 
-    const data = [];
+    const speed = [];
     for (const [obs, fcst, coord] of zip(
       loop.observation.magnitude,
       loop.forecast.magnitude,
       loop.coords
     )) {
-      data.push([...coord, obs - fcst]);
+      speed.push([...coord, obs - fcst]);
     }
 
     const x = scaleLinear(
-      extent(data, (d) => d[0]),
+      extent(speed, (d) => d[0]),
       [0, width]
     );
     const y = scaleLinear(
-      extent(data, (d) => d[1]),
+      extent(speed, (d) => d[1]),
       [height, 0]
     );
 
-    const [minVal, maxVal] = extent(data, (d) => d[2]);
+    const [minVal, maxVal] = extent(speed, (d) => d[2]);
     const fill = scaleDiverging([minVal, 0, maxVal], interpolateRdBu);
 
     svg
       .selectAll("circle")
-      .data(data)
+      .data(speed)
       .join("circle")
       .attr("r", 4)
       .attr("cx", (d) => x(d[0]))
       .attr("cy", (d) => y(d[1]))
       .attr("fill", (d) => fill(d[2]));
+
+    const obs = Array.from(zip(loop.observation.direction, loop.coords));
+
+    svg
+      .selectAll(".observation")
+      .data(obs)
+      .join("line")
+      .attr("stroke", "black")
+      .attr("stroke-width", "1px")
+      .attr("x1", (d) => x(d[1][0]))
+      .attr("y1", (d) => y(d[1][1]))
+      .attr("x2", (d) => x(d[1][0]) + 12 * Math.cos(((d[0] + 90) * Math.PI) / 180))
+      .attr("y2", (d) => y(d[1][1]) + 12 * Math.sin(((d[0] + 90) * Math.PI) / 180));
+
+    const fcst = Array.from(zip(loop.forecast.direction, loop.coords));
+
+    svg
+      .selectAll(".forecast")
+      .data(fcst)
+      .join("line")
+      .attr("stroke", "black")
+      .attr("stroke-width", "1px")
+      .attr("x1", (d) => x(d[1][0]))
+      .attr("y1", (d) => y(d[1][1]))
+      .attr("x2", (d) => x(d[1][0]) + 6 * Math.cos(((d[0] + 90) * Math.PI) / 180))
+      .attr("y2", (d) => y(d[1][1]) + 6 * Math.sin(((d[0] + 90) * Math.PI) / 180));
   }
 }
