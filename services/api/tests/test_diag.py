4d1b994b1c85199337736143749ad248322f3b68
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-07-30 11:00:12 -0600

    Refator test.diag to return a single list of floats

--- services/api/tests/test_diag.py
+++ services/api/tests/test_diag.py
@@ -20,50 +20,33 @@ def make_scalar_diag():
     return _make_scalar_diag
 
 
-@mock.patch("unified_graphics.diag.ScalarDiag", autospec=True)
 @mock.patch("unified_graphics.diag.open_diagnostic", autospec=True)
 @pytest.mark.parametrize("loop", ([diag.MinimLoop.GUESS], [diag.MinimLoop.ANALYSIS]))
-def test_temperature(mock_open_diagnostic, mock_ScalarDiag, loop):
-    expected = diag.ScalarDiag(
-        bins=[diag.Bin(lower=-1, upper=1, value=3)], observations=10, std=1.2, mean=0.3
-    )
-
+def test_temperature(mock_open_diagnostic, loop):
     mock_open_diagnostic.return_value = xr.Dataset(
-        {"Obs_Minus_Forecast_adjusted": xr.DataArray([1, 3, 5])}
+        {"Obs_Minus_Forecast_unadjusted": xr.DataArray([1, 3, 5])}
     )
-    mock_ScalarDiag.from_array.return_value = expected
 
     result = diag.temperature(loop)
 
     mock_open_diagnostic.assert_called_once_with(diag.Variable.TEMPERATURE, loop)
-    mock_ScalarDiag.from_array.assert_called_once()
-    xr.testing.assert_equal(
-        mock_ScalarDiag.from_array.call_args_list[0].args[0], xr.DataArray([1, 3, 5])
-    )
-
-    assert result == expected
+    assert result == [1.0, 3.0, 5.0]
 
 
-@mock.patch("unified_graphics.diag.ScalarDiag", autospec=True)
 @mock.patch("unified_graphics.diag.open_diagnostic", autospec=True)
-def test_temperature_diag_does_not_exist(mock_open_diagnostic, mock_ScalarDiag):
+def test_temperature_diag_does_not_exist(mock_open_diagnostic):
     mock_open_diagnostic.side_effect = FileNotFoundError()
 
     with pytest.raises(FileNotFoundError):
         diag.temperature(diag.MinimLoop.GUESS)
 
-    mock_ScalarDiag.from_array.assert_not_called()
 
-
-@mock.patch("unified_graphics.diag.ScalarDiag", autospec=True)
 @mock.patch("unified_graphics.diag.open_diagnostic", autospec=True)
-def test_temperature_diag_unknown_backend(mock_open_diagnostic, mock_ScalarDiag):
+def test_temperature_diag_unknown_backend(mock_open_diagnostic):
     mock_open_diagnostic.side_effect = ValueError()
 
     with pytest.raises(ValueError):
         diag.temperature(diag.MinimLoop.GUESS)
-
-    mock_ScalarDiag.from_array.assert_not_called()
 
 
 @pytest.mark.parametrize(
@@ -221,46 +204,6 @@ def test_coordinate_pairs_from_empty_vectors():
 def test_coordinate_pairs_from_mismatched_vectors(lng, lat):
     with pytest.raises(AssertionError):
         diag.coordinate_pairs_from_vectors(xr.DataArray(lng), xr.DataArray(lat))
-
-
-def test_ScalarDiag_from_array():
-    data = xr.DataArray([10.0, 8.0, 13.0, 9.0, 11.0, 14.0, 6.0, 4.0, 12.0, 7.0, 5.0])
-    expected_obs = len(data)
-    expected_mean = sum(data) / expected_obs
-    expected_std = math.sqrt(
-        sum([(x - expected_mean) ** 2 for x in data]) / expected_obs
-    )
-
-    result = diag.ScalarDiag.from_array(data)
-
-    assert result == diag.ScalarDiag(
-        bins=[
-            diag.Bin(lower=4, upper=6, value=2),
-            diag.Bin(lower=6, upper=8, value=2),
-            diag.Bin(lower=8, upper=10, value=2),
-            diag.Bin(lower=10, upper=12, value=2),
-            diag.Bin(lower=12, upper=14, value=3),
-        ],
-        observations=expected_obs,
-        mean=expected_mean,
-        std=expected_std,
-    )
-
-
-def test_ScalarDiag_from_zeros():
-    data = xr.DataArray([0, 0, 0, 0])
-
-    result = diag.ScalarDiag.from_array(data)
-
-    assert result == diag.ScalarDiag(
-        bins=[diag.Bin(lower=-0.5, upper=0.5, value=4)], observations=4, mean=0, std=0
-    )
-
-
-def test_ScalarDiag_from_empty_array():
-    result = diag.ScalarDiag.from_array(xr.DataArray([]))
-
-    assert result == diag.ScalarDiag(bins=[], observations=0, mean=0, std=0)
 
 
 def test_VectorVariable_from_vectors():
