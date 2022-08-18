c86ab810794e7211c18ad84c9bbc1a507c16facc
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-08-18 13:24:42 -0600

    Add some space between elements in the header

--- services/ui/src/js/components/Header/Header.js
+++ services/ui/src/js/components/Header/Header.js
@@ -2,14 +2,22 @@ import { html } from "htm/preact";
 
 export default function Header() {
   return html`<header
-    class="usa-dark-background display-flex flex-justify-center padding-y-2 padding-x-3"
+    class="usa-dark-background display-flex flex-justify gap-2 padding-y-2 padding-x-3"
   >
-    <strong>Unified Graphics</strong>
-    <select>
-      <option>RRFS</option>
-    </select>
-    <select>
-      <option>05 May 2022 14:00</option>
-    </select>
+    <strong><span>Unified Graphics</span></strong>
+    <form class="display-flex gap-2">
+      <select>
+        <option>RRFS</option>
+      </select>
+      <select>
+        <option>05 May 2022 14:00</option>
+      </select>
+    </form>
+    <nav>
+      <ul class="add-list-reset display-flex gap-2">
+        <li><a href="#">Diagnostics</a></li>
+        <li><a href="#">Model Output</a></li>
+      </ul>
+    </nav>
   </header>`;
 }
