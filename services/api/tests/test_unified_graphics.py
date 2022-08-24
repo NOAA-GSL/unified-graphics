fc788aba20529eed623819d01c934032fba48d2b
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-08-24 08:52:55 -0600

    Convert remaining route tests to integration tests
    I've removed all mocks from test_unified_graphics so that all of the
    tests of our endpoints are integration tests. This should make it easier
    to do some refactoring of the vector variable diagnostics.

--- services/api/tests/test_unified_graphics.py
+++ services/api/tests/test_unified_graphics.py
@@ -1,14 +1,7 @@
 from pathlib import Path
-from unittest import mock
 
 import pytest  # noqa: F401
 import xarray as xr
-
-from unified_graphics.diag import (
-    Coordinate,
-    PolarCoordinate,
-    VectorObservation,
-)
 
 
 def test_root_endpoint(client):
@@ -79,45 +72,37 @@ def test_scalar_diag(variable_name, variable_code, diag_file, client):
     }
 
 
-@pytest.mark.parametrize("variable_name", ["temperature", "moisture", "pressure"])
-def test_scalar_diag_not_found(variable_name, client):
-    response = client.get(f"/diag/{variable_name}/")
-
-    assert response.status_code == 404
-    assert response.json == {"msg": "Diagnostic file not found"}
-
-
-@pytest.mark.parametrize(
-    "variable_name,variable_code",
-    [("temperature", "t"), ("moisture", "q"), ("pressure", "p")],
-)
-def test_scalar_diag_read_error(variable_name, variable_code, app, client):
-    empty = (
-        Path(app.config["DIAG_DIR"]) / f"ncdiag_conv_{variable_code}_ges.nc4.2022050514"
+def test_wind_diag(diag_file, client):
+    diag_file(
+        "ncdiag_conv_uv_ges.nc4.2022050514",
+        xr.Dataset(
+            {
+                "Station_ID": xr.DataArray([b"WV270   ", b"E4294   "]),
+                "u_Observation": xr.DataArray([1, 0]),
+                "v_Observation": xr.DataArray([0, 1]),
+                "u_Obs_Minus_Forecast_adjusted": xr.DataArray([0.5, 0]),
+                "v_Obs_Minus_Forecast_adjusted": xr.DataArray([0, 0.5]),
+                "Longitude": xr.DataArray([240, 272]),
+                "Latitude": xr.DataArray([40, 30]),
+            }
+        ),
     )
-    empty.touch()
+    diag_file(
+        "ncdiag_conv_uv_anl.nc4.2022050514",
+        xr.Dataset(
+            {
+                "Station_ID": xr.DataArray([b"WV270   ", b"E4294   "]),
+                "u_Observation": xr.DataArray([1, 0]),
+                "v_Observation": xr.DataArray([0, 1]),
+                "u_Obs_Minus_Forecast_adjusted": xr.DataArray([0.1, 0]),
+                "v_Obs_Minus_Forecast_adjusted": xr.DataArray([0, 0.1]),
+                "Longitude": xr.DataArray([240, 272]),
+                "Latitude": xr.DataArray([40, 30]),
+            }
+        ),
+    )
 
-    response = client.get(f"/diag/{variable_name}/")
-
-    assert response.status_code == 500
-    assert response.json == {"msg": "Unable to read diagnostic file"}
-
-
-@mock.patch("unified_graphics.diag.wind", autospec=True)
-def test_wind_diag(mock_diag_wind, client):
-    mock_diag_wind.return_value = [
-        VectorObservation(
-            "WV270",
-            "wind",
-            PolarCoordinate(1, 180),
-            PolarCoordinate(0.5, 0),
-            PolarCoordinate(1, 90),
-            Coordinate(0, 0),
-        )
-    ]
     response = client.get("/diag/wind/")
-
-    mock_diag_wind.assert_called_once()
 
     assert response.status_code == 200
     assert response.json == {
@@ -128,21 +113,48 @@ def test_wind_diag(mock_diag_wind, client):
                 "properties": {
                     "stationId": "WV270",
                     "variable": "wind",
-                    "guess": {"magnitude": 1, "direction": 180},
-                    "analysis": {"magnitude": 0.5, "direction": 0},
-                    "observed": {"magnitude": 1, "direction": 90},
+                    "guess": {"magnitude": 0.5, "direction": 270.0},
+                    "analysis": {"magnitude": 0.1, "direction": 270.0},
+                    "observed": {"magnitude": 1.0, "direction": 270.0},
                 },
-                "geometry": {"type": "Point", "coordinates": [0, 0]},
-            }
+                "geometry": {"type": "Point", "coordinates": [-120.0, 40.0]},
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "stationId": "E4294",
+                    "variable": "wind",
+                    "guess": {"magnitude": 0.5, "direction": 180.0},
+                    "analysis": {"magnitude": 0.1, "direction": 180.0},
+                    "observed": {"magnitude": 1.0, "direction": 180.0},
+                },
+                "geometry": {"type": "Point", "coordinates": [-88.0, 30.0]},
+            },
         ],
     }
 
 
-@mock.patch("unified_graphics.diag.wind", autospec=True)
-def test_wind_diag_read_error(mock_diag_wind, client):
-    mock_diag_wind.side_effect = ValueError()
+@pytest.mark.parametrize(
+    "variable_name", ["temperature", "moisture", "pressure", "wind"]
+)
+def test_diag_not_found(variable_name, client):
+    response = client.get(f"/diag/{variable_name}/")
 
-    response = client.get("/diag/wind/")
+    assert response.status_code == 404
+    assert response.json == {"msg": "Diagnostic file not found"}
+
+
+@pytest.mark.parametrize(
+    "variable_name,variable_code",
+    [("temperature", "t"), ("moisture", "q"), ("pressure", "p"), ("wind", "uv")],
+)
+def test_diag_read_error(variable_name, variable_code, app, client):
+    empty = (
+        Path(app.config["DIAG_DIR"]) / f"ncdiag_conv_{variable_code}_ges.nc4.2022050514"
+    )
+    empty.touch()
+
+    response = client.get(f"/diag/{variable_name}/")
 
     assert response.status_code == 500
     assert response.json == {"msg": "Unable to read diagnostic file"}
