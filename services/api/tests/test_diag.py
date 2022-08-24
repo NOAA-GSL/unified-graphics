943869408680d76b929df4ec7044052d944692ee
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-08-24 09:12:10 -0600

    Remove mocks from test_diag
    Removed some unneeded tests and the mocks from the test_diag unit tests.
    These tests are unit tests that focus on the open_diagnostic function
    now.

--- services/api/tests/test_diag.py
+++ services/api/tests/test_diag.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from unittest import mock
 
 import pytest
 import xarray as xr
@@ -71,77 +70,3 @@ def test_open_diagnostic_unknown_backend(app):
     with app.app_context():
         with pytest.raises(ValueError, match=expected):
             diag.open_diagnostic(diag.Variable.WIND, diag.MinimLoop.GUESS)
-
-
-@mock.patch("unified_graphics.diag.open_diagnostic", autospec=True)
-def test_wind(mock_open_diagnostic):
-    # Arrange
-
-    test_dataset = xr.Dataset(
-        {
-            "Station_ID": xr.DataArray([b"WV270   ", b"E4294   "]),
-            "u_Observation": xr.DataArray([0, 0]),
-            "u_Obs_Minus_Forecast_adjusted": xr.DataArray([3, 1]),
-            "v_Observation": xr.DataArray([10, 10]),
-            "v_Obs_Minus_Forecast_adjusted": xr.DataArray([-4, 0]),
-            "Longitude": xr.DataArray([240, 272]),
-            "Latitude": xr.DataArray([40, 30]),
-        }
-    )
-
-    mock_open_diagnostic.return_value = test_dataset
-
-    # Act
-
-    data = diag.wind()
-
-    # Assert
-
-    assert mock_open_diagnostic.mock_calls == [
-        mock.call(diag.Variable.WIND, diag.MinimLoop.GUESS),
-        mock.call(diag.Variable.WIND, diag.MinimLoop.ANALYSIS),
-    ]
-
-    assert data == [
-        diag.VectorObservation(
-            stationId="WV270",
-            variable="wind",
-            guess=diag.PolarCoordinate(5.0, 323.13010235415595),
-            analysis=diag.PolarCoordinate(5.0, 323.13010235415595),
-            observed=diag.PolarCoordinate(10.0, 180.0),
-            position=diag.Coordinate(-120.0, 40.0),
-        ),
-        diag.VectorObservation(
-            stationId="E4294",
-            variable="wind",
-            guess=diag.PolarCoordinate(1.0, 270.0),
-            analysis=diag.PolarCoordinate(1.0, 270.0),
-            observed=diag.PolarCoordinate(10.0, 180.0),
-            position=diag.Coordinate(-88.0, 30.0),
-        ),
-    ]
-
-
-def test_VectorObservation_to_geojson():
-    subject = diag.VectorObservation(
-        "WV270",
-        "wind",
-        diag.PolarCoordinate(1, 180),
-        diag.PolarCoordinate(0.5, 0),
-        diag.PolarCoordinate(1, 90),
-        diag.Coordinate(0, 0),
-    )
-
-    result = subject.to_geojson()
-
-    assert result == {
-        "type": "Feature",
-        "properties": {
-            "stationId": "WV270",
-            "variable": "wind",
-            "guess": {"magnitude": 1, "direction": 180},
-            "analysis": {"magnitude": 0.5, "direction": 0},
-            "observed": {"magnitude": 1, "direction": 90},
-        },
-        "geometry": {"type": "Point", "coordinates": [0, 0]},
-    }
