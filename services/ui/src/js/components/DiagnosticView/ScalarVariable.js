7cd2abc8f206809ea39b8bf172f5831bbc09ac4e
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-08-25 14:43:27 -0600

    Restore the scalar display to a working state
    Scalar variables display on the map and the brushing works. Still need
    to sync the axes in the histograms and the scales of the maps so that
    everything is comparable.

--- services/ui/src/js/components/DiagnosticView/ScalarVariable.js
+++ services/ui/src/js/components/DiagnosticView/ScalarVariable.js
@@ -1,42 +1,50 @@
 import { html } from "htm/preact";
 import { useState } from "preact/hooks";
 
-import useBrushedScalar from "./useBrushedScalar";
-import VariableDisplay from "./VariableDisplay";
+import { geoFilter } from "./DiagnosticView.helpers";
 
 export default function ScalarVariable(props) {
   const [selection, setSelection] = useState(null);
 
-  const [guess, setGuessBrush] = useBrushedScalar({
-    features: props.featureCollection.features,
-    loop: "guess",
-  });
-  const [analysis, setAnalysisBrush] = useBrushedScalar({
-    features: props.featureCollection.features,
-    loop: "analysis",
-  });
-
   const brushCallback = (event) => {
     setSelection(event.detail);
-    setGuessBrush(event.detail);
-    setAnalysisBrush(event.detail);
   };
 
-  return html` <h2>Guess</h2>
-    <${VariableDisplay}
-      loop="guess"
-      distribution=${guess}
-      observations=${props.featureCollection}
-      selection=${selection}
-      brushCallback=${brushCallback}
-    />
+  const data = props.featureCollection.features.filter(geoFilter(selection));
+  const guess = data.map((feature) => feature.properties.guess);
+  const analysis = data.map((feature) => feature.properties.analysis);
+
+  return html`<h2>Guess</h2>
+    <div data-layout="cluster">
+      <chart-histogram
+        class="flex-1"
+        data=${guess}
+        title-x="Observation − Forecast"
+        title-y="Observation Count"
+      ></chart-histogram>
+      <chart-map
+        class="flex-1"
+        data=${props.featureCollection}
+        selection=${selection}
+        radius=${(feature) => feature.properties.guess}
+        onchart-brush=${brushCallback}
+      ></chart-map>
+    </div>
 
     <h2>Analysis</h2>
-    <${VariableDisplay}
-      loop="analysis"
-      distribution=${analysis}
-      observations=${props.featureCollection}
-      selection=${selection}
-      brushCallback=${brushCallback}
-    />`;
+    <div data-layout="cluster">
+      <chart-histogram
+        class="flex-1"
+        data=${analysis}
+        title-x="Observation − Forecast"
+        title-y="Observation Count"
+      ></chart-histogram>
+      <chart-map
+        class="flex-1"
+        data=${props.featureCollection}
+        selection=${selection}
+        radius=${(feature) => feature.properties.analysis}
+        onchart-brush=${brushCallback}
+      ></chart-map>
+    </div>`;
 }
