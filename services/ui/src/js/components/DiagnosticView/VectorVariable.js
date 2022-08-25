5c8a2a6e7f1779c4a2d57e423ec6561d93a38f64
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-08-25 14:57:57 -0600

    Implement basic vector view
    Copied over the code for the ScalarVariable component into
    VectorVariable and updated it to access the properties on the vector
    variable. For now it's just using a histogram for the distribution,
    because we have another issue open to implement the contour density to
    show distributions of 2D vectors.
    
    There are some errors in the console about getting NaN in some of the
    SVG attributes when using the histogram with a vector variable. Not sure
    what that's about, but it hopefully won't matter once I swap this out
    for a contour density.

--- services/ui/src/js/components/DiagnosticView/VectorVariable.js
+++ services/ui/src/js/components/DiagnosticView/VectorVariable.js
@@ -1,5 +1,50 @@
 import { html } from "htm/preact";
+import { useState } from "preact/hooks";
 
-export default function VectorVariable() {
-  return html`<p>Test</p>`;
+import { geoFilter } from "./DiagnosticView.helpers";
+
+export default function VectorVariable(props) {
+  const [selection, setSelection] = useState(null);
+
+  const brushCallback = (event) => {
+    setSelection(event.detail);
+  };
+
+  const data = props.featureCollection.features.filter(geoFilter(selection));
+  const guess = data.map((feature) => feature.properties.guess.magnitude);
+  const analysis = data.map((feature) => feature.properties.analysis.magnitude);
+
+  return html`<h2>Guess</h2>
+    <div data-layout="cluster">
+      <chart-histogram
+        class="flex-1"
+        data=${guess}
+        title-x="Observation − Forecast"
+        title-y="Observation Count"
+      ></chart-histogram>
+      <chart-map
+        class="flex-1"
+        data=${props.featureCollection}
+        selection=${selection}
+        radius=${(feature) => feature.properties.guess.magnitude}
+        onchart-brush=${brushCallback}
+      ></chart-map>
+    </div>
+
+    <h2>Analysis</h2>
+    <div data-layout="cluster">
+      <chart-histogram
+        class="flex-1"
+        data=${analysis}
+        title-x="Observation − Forecast"
+        title-y="Observation Count"
+      ></chart-histogram>
+      <chart-map
+        class="flex-1"
+        data=${props.featureCollection}
+        selection=${selection}
+        radius=${(feature) => feature.properties.analysis.magnitude}
+        onchart-brush=${brushCallback}
+      ></chart-map>
+    </div>`;
 }
