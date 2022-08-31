f5781f5978d16fc953afa160aa86c917ec3bb17f
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-08-31 09:20:08 -0600

    Handle calm vectors in vector_direction
    Because numpy.arctan2 treats 0.0 and -0.0 differently, we have to
    normalize the output of our vector_direction function whenever we detect
    a "calm" (magnitude 0) vector.

--- services/api/tests/test_diag.py
+++ services/api/tests/test_diag.py
@@ -73,17 +73,27 @@ def test_open_diagnostic_unknown_backend(app):
             diag.open_diagnostic(diag.Variable.WIND, diag.MinimLoop.GUESS)
 
 
-def test_vector_direction():
-    # Test cases taken from the examples at
-    # http://ncl.ucar.edu/Document/Functions/Contributed/wind_direction.shtml
-    u = np.array([10, 0, 0, -10, 10, 10, -10, -10])
-    v = np.array([0, 10, -10, 0, 10, -10, 10, -10])
-
+# Test cases taken from the examples at
+# http://ncl.ucar.edu/Document/Functions/Contributed/wind_direction.shtml
+@pytest.mark.parametrize(
+    "u,v,expected",
+    (
+        [
+            np.array([10, 0, 0, -10, 10, 10, -10, -10]),
+            np.array([0, 10, -10, 0, 10, -10, 10, -10]),
+            np.array([270, 180, 0, 90, 225, 315, 135, 45]),
+        ],
+        [
+            np.array([0.0, -0.0, 0.0, -0.0]),
+            np.array([0.0, 0.0, -0.0, -0.0]),
+            np.array([0.0, 0.0, 0.0, 0.0]),
+        ],
+    ),
+)
+def test_vector_direction(u, v, expected):
     result = diag.vector_direction(u, v)
 
-    np.testing.assert_array_almost_equal(
-        result, np.array([270, 180, 0, 90, 225, 315, 135, 45]), decimal=5
-    )
+    np.testing.assert_array_almost_equal(result, expected, decimal=5)
 
 
 def test_vector_magnitude():
