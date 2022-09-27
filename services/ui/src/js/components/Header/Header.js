56fdfb7eea8f620b16d3e5d3574f87b5a90b6e26
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-09-27 13:26:12 -0600

    Replace inline utilities with a cluster layout
    Just because I can create a cluster layout with utilities, doesn't mean
    I should. I had the cluster layout in my CSS, why not use it.

--- services/ui/src/js/components/Header/Header.js
+++ services/ui/src/js/components/Header/Header.js
@@ -2,7 +2,9 @@ import { html } from "htm/preact";
 
 export default function Header() {
   return html`<header
-    class="usa-dark-background display-flex flex-justify gap-2 padding-y-2 padding-x-3"
+    class="usa-dark-background padding-y-2 padding-x-3"
+    style="justify-content: space-between"
+    data-layout="cluster"
   >
     <strong><span>Unified Graphics</span></strong>
     <form class="display-flex gap-2">
@@ -14,7 +16,7 @@ export default function Header() {
       </select>
     </form>
     <nav>
-      <ul class="add-list-reset display-flex gap-2">
+      <ul data-layout="cluster" role="list">
         <li><a href="#">Diagnostics</a></li>
         <li><a href="#">Model Output</a></li>
       </ul>
