ffe5b59831e14fd92d7b5aa7a8156764f56ba004
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-10-06 10:29:10 -0600

    Remove DiagScalar and DataSource
    These are old, unused components.

--- services/ui/src/js/main.js
+++ services/ui/src/js/main.js
@@ -1,7 +1,5 @@
 import App from "./App.svelte";
 
-import DataSource from "./components/DataSource";
-import DiagScalar from "./components/DiagScalar";
 import { Chart2DHistogram, ChartDensity, ChartHistogram } from "./components/Chart";
 import ChartMap from "./components/ChartMap";
 
@@ -9,8 +7,6 @@ customElements.define("chart-2dhistogram", Chart2DHistogram);
 customElements.define("chart-histogram", ChartHistogram);
 customElements.define("chart-density", ChartDensity);
 customElements.define("chart-map", ChartMap);
-customElements.define("data-source", DataSource);
-customElements.define("diag-scalar", DiagScalar);
 
 const app = new App({
   target: document.body,
