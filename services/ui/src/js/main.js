47be2f6fcb93d10a86d2e22017dfda52a307c4df
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-10-11 13:09:08 -0600

    Remove ChartDensity import, since we’re not using it
    This will shrink our bundle size a little, I think, since we’re not
    using the chart at the moment.

--- services/ui/src/js/main.js
+++ services/ui/src/js/main.js
@@ -1,11 +1,10 @@
 import App from "./App.svelte";
 
-import { Chart2DHistogram, ChartDensity, ChartHistogram } from "./components/Chart";
+import { Chart2DHistogram, ChartHistogram } from "./components/Chart";
 import ChartMap from "./components/ChartMap";
 
 customElements.define("chart-2dhistogram", Chart2DHistogram);
 customElements.define("chart-histogram", ChartHistogram);
-customElements.define("chart-density", ChartDensity);
 customElements.define("chart-map", ChartMap);
 
 const app = new App({
