bc300f4aae39bd626c91b2f676d4b422642d130d
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-10-11 16:20:09 -0600

    Reduce ChartElement to just some default props
    The base ChartElement no longer provides any template markup or resize
    handling. It exposes a height and width attribute (and property) that
    trigger a re-render. The width and height can be set by putting the
    chart inside a ChartContainer.
    
    This should make it easier to use the base ChartElement with the map as
    well as the SVG charts.

--- services/ui/src/js/components/Chart/ChartElement.js
+++ services/ui/src/js/components/Chart/ChartElement.js
@@ -1,202 +1,54 @@
 class ChartElement extends HTMLElement {
-  static #TEMPLATE = `<span id="title-y"></span>
-  <div class="container">
-    <svg>
-      <g class="x-axis"></g>
-      <g class="y-axis"></g>
-      <g class="data"></g>
-      <g class="annotation"></g>
-    </svg>
-  </div>
-  <span id="title-x"></span>`;
-
-  static #STYLE = `:host {
-      display: grid;
-      gap: 0.5em;
-      grid-template-columns: min-content 1fr;
-      grid-template-rows: 1fr min-content;
-      place-items: center;
-    }
-
-    :host,
-    .container {
-      contain: strict;
-    }
-
-    #title-x,
-    svg {
-      grid-column: 2 / 3;
-    }
-
-    #title-y,
-    svg {
-      grid-row: 1 / 2;
-    }
-
-    #title-y {
-      writing-mode: vertical-lr;
-      transform: rotate(180deg);
-    }
-
-    #title-x,
-    .x-axis {
-      color: #3d4551;
-    }
-
-    #title-y,
-    .y-axis {
-      color: #71767a;
-    }
-
-    .container {
-      place-self: stretch;
-    }
-
-    line,
-    path,
-    rect {
-      shape-rendering: crispEdges;
-    }`;
-
-  #data = [];
-  #range = null;
   #pendingUpdate = null;
 
   static get observedAttributes() {
-    return ["format-x", "format-y", "src", "title-x", "title-y"];
+    return ["width", "height"];
   }
 
-  constructor() {
-    super();
-
-    const shadowRoot = this.attachShadow({ mode: "open" });
-    shadowRoot.innerHTML = `<style>${this.componentStyles}</style>${
-      ChartElement.#TEMPLATE
-    }`;
-  }
-
-  connectedCallback() {
-    this.resizeObserver = new ResizeObserver(() => {
-      this.requestUpdate();
-    });
-    this.resizeObserver.observe(this.shadowRoot?.querySelector(".container"));
-  }
-
-  disconnectedCallback() {
-    this?.resizeObserver.unobserve(this.shadowRoot?.querySelector(".container"));
-    delete this?.resizeObserver;
-  }
-
-  attributeChangedCallback(name, oldValue, newValue) {
+  attributeChangedCallback(name) {
     switch (name) {
-      case "src":
-        fetch(newValue)
-          .then((response) => response.json())
-          .then((data) => {
-            this.data = data;
-            this.dispatchEvent(new CustomEvent("data-load", { bubbles: true }));
-          })
-          .catch((err) => {
-            console.error(err);
-          });
-        break;
-      case "format-x":
-      case "format-y":
-        this.requestUpdate();
-        break;
-      case "title-x":
-      case "title-y":
-        this.#updateLabel(name, newValue);
+      case "height":
+      case "width":
+        this.update();
         break;
       default:
         break;
     }
   }
 
-  get componentStyles() {
-    return ChartElement.#STYLE;
+  get height() {
+    const v = this.getAttribute("height");
+
+    if (v) return +v;
+
+    return undefined;
   }
 
-  get data() {
-    return structuredClone(this.#data);
-  }
-
-  set data(value) {
-    this.#data = value;
-    this.requestUpdate();
-  }
-
-  get formatX() {
-    return this.getAttribute("format-x") ?? ",";
-  }
-
-  set formatX(value) {
+  set height(value) {
     if (!value) {
-      this.removeAttribute("format-x");
+      this.removeAttribute("height");
     } else {
-      this.setAttribute("format-x", value);
+      this.setAttribute("height", value.toString());
     }
   }
 
-  get formatY() {
-    return this.getAttribute("format-y") ?? ",";
+  get width() {
+    const v = this.getAttribute("width");
+
+    if (v) return +v;
+
+    return undefined;
   }
 
-  set formatY(value) {
+  set width(value) {
     if (!value) {
-      this.removeAttribute("format-y");
+      this.removeAttribute("width");
     } else {
-      this.setAttribute("format-y", value);
+      this.setAttribute("width", value.toString());
     }
   }
 
-  get range() {
-    return structuredClone(this.#range);
-  }
-
-  set range(value) {
-    this.#range = value;
-    this.requestUpdate();
-  }
-
-  get src() {
-    return this.getAttribute("src");
-  }
-
-  set src(value) {
-    if (!value) {
-      this.removeAttribute("src");
-      this.data = [];
-    } else {
-      this.setAttribute("src", value);
-    }
-  }
-
-  get titleX() {
-    return this.getAttribute("title-x");
-  }
-
-  set titleX(value) {
-    if (!value) {
-      this.removeAttribute("title-x");
-    } else {
-      this.setAttribute("title-x", value);
-    }
-  }
-
-  get titleY() {
-    return this.getAttribute("title-y");
-  }
-
-  set titleY(value) {
-    if (!value) {
-      this.removeAttribute("title-y");
-    } else {
-      this.setAttribute("title-y", value);
-    }
-  }
-
-  requestUpdate() {
+  update() {
     if (this.#pendingUpdate) return;
 
     this.#pendingUpdate = window.requestAnimationFrame(() => {
@@ -206,12 +58,6 @@ class ChartElement extends HTMLElement {
   }
 
   render() {}
-
-  #updateLabel(id, value) {
-    const span = this.shadowRoot?.getElementById(id);
-    if (!span) return;
-    span.textContent = value;
-  }
 }
 
 export default ChartElement;
