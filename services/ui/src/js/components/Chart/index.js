79c0d8e57aff1e746f2c7489f7a52263edea79fd
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-10-12 15:53:23 -0600

    Remove the ChartDensity component
    This is unused, and rather than refactor it to fit the new ChartElement
    base class (since we can't test it easily), I'm just going to remove it.

--- services/ui/src/js/components/Chart/index.js
+++ services/ui/src/js/components/Chart/index.js
@@ -1,3 +1,2 @@
 export { default as Chart2DHistogram } from "./Chart2DHistogram";
-export { default as ChartDensity } from "./ChartDensity";
 export { default as ChartHistogram } from "./ChartHistogram";
