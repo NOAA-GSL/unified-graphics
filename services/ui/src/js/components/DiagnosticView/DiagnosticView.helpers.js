83afa8620ac9336c156a18b13ef894da5601a49c
W. Evan Sheehan <evan.sheehan@noaa.gov>
2022-10-19 14:26:30 -0600

    Handle multiple dimensions in containedIn

--- services/ui/src/js/components/DiagnosticView/DiagnosticView.helpers.js
+++ services/ui/src/js/components/DiagnosticView/DiagnosticView.helpers.js
@@ -1,4 +1,4 @@
-export function containedIn(range, prop) {
+export function containedIn(range, loop) {
   function filter(feature) {
     // If no range is specified, allow all points
     if (!range) return true;
@@ -6,17 +6,38 @@ export function containedIn(range, prop) {
     // If this feature is not a point, exclude it
     if (feature.geometry.type !== "Point") return false;
 
-    const val = feature.properties[prop];
+    // If range is an array, then we're dealing with a scalar variable.
+    // Otherwise, we're filtering on multiple dimensions and need to test each
+    // point to ensure it falls within the range of every dimensions.
+    if (Array.isArray(range)) {
+      const val = feature.properties[loop];
 
-    if (val === undefined) return false;
+      const lower = Math.min(...range);
+      const upper = Math.max(...range);
 
-    const lower = Math.min(...range);
-    const upper = Math.max(...range);
+      // Include this point if the range is effectively 0
+      if (lower === upper) return true;
 
-    if (lower === upper) return true;
+      // Exclude the point if it doesn't have a value for loop, or if it falls
+      // outside the range
+      if (val === undefined || val < lower || val > upper) return false;
+    } else {
+      // Range is not an array, so need to iterate over the properties in the
+      // object and check our point for each property.
+      for (let [name, [lower, upper]] of Object.entries(range)) {
+        const val = feature.properties[loop][name];
 
-    if (val < lower || val > upper) return false;
+        // If the range for this dimension is 0, we won't reject the point
+        // based this dimension.
+        if (lower === upper) continue;
 
+        // Exlcude this point if it doesn't have this dimension, or if its
+        // value falls outside of the range.
+        if (val === undefined || val < lower || val > upper) return false;
+      }
+    }
+
+    // If we've made it this far, we can include this point.
     return true;
   }
 
