04219fa420c921145379d6d205b3a03b0aa6bd6d
Ian McGinnis <67600557+ian-noaa@users.noreply.github.com>
2022-11-09 11:46:48 -0700

    Consolidate the S3 test setup into a fixture
    We had a lot of repetitive setup in the S3 tests. Move the common
    components into the fixture that creates the S3 bucket.

--- services/api/tests/test_diag.py
+++ services/api/tests/test_diag.py
@@ -2,14 +2,12 @@ import os
 import time
 from pathlib import Path
 
-import fsspec
 import numpy as np
 import pytest
 import requests
 import xarray as xr
-from s3fs import S3FileSystem
 
-from unified_graphics import diag
+from unified_graphics import diag, create_app
 
 # Global resources for s3
 test_bucket_name = "test"
@@ -59,8 +57,8 @@ def s3_base():
     try:
         print("polling done. testing if server is up")
         r = requests.get(endpoint_uri)
-    except Exception:
-        raise RuntimeError("Moto server not up")
+    except Exception as e:
+        raise RuntimeError("Moto server not up") from e
     else:
         if r.ok:
             print("server up")
@@ -77,11 +75,16 @@ def get_boto3_client():
 
 
 @pytest.fixture()
-def s3(s3_base):
+def s3_app(s3_base, tmp_path):
     client = get_boto3_client()
     client.create_bucket(Bucket=test_bucket_name, ACL="public-read")
 
-    yield
+    app = create_app()
+    app.config["DIAG_DIR"] = f"s3://{test_bucket_name}/"
+    app.config["ENDPOINT_URL"] = endpoint_uri
+    app.config["LOCAL_CACHE_PATH"] = f"{tmp_path}/cache"
+
+    yield app
 
 
 @pytest.fixture
@@ -160,53 +163,37 @@ def test_open_diagnostic_unknown_uri(app):
             diag.open_diagnostic(diag.Variable.WIND, diag.MinimLoop.GUESS)
 
 
-# TODO - Consider making a "s3_app" fixture that combines s3 and app fixtures?
-# TODO - This could remove some of the DIAG_DIR, ENDPOINT_URL, and LOCAL_CACHE_PATH
-# TODO - throat clearing.
-def test_open_diagnostic_s3_nonexistent_bucket(app, s3, tmp_path):
-    app.config["DIAG_DIR"] = "s3://foo/"
-    app.config["ENDPOINT_URL"] = endpoint_uri
-    app.config["LOCAL_CACHE_PATH"] = f"{tmp_path}/cache"
+def test_open_diagnostic_s3_nonexistent_bucket(s3_app):
+    s3_app.config["DIAG_DIR"] = "s3://foo/"
 
     expected = r"The specified bucket does not exist"
 
-    with app.app_context():
+    with s3_app.app_context():
         with pytest.raises(FileNotFoundError, match=expected):
             diag.open_diagnostic(diag.Variable.WIND, diag.MinimLoop.GUESS)
 
 
-def test_open_diagnostic_s3_nonexistent_key(app, s3, tmp_path):
-    app.config["DIAG_DIR"] = f"s3://{test_bucket_name}/"
-    app.config["ENDPOINT_URL"] = endpoint_uri
-    app.config["LOCAL_CACHE_PATH"] = f"{tmp_path}/cache"
-
+def test_open_diagnostic_s3_nonexistent_key(s3_app):
     expected = r"The specified key does not exist"
 
-    with app.app_context():
+    with s3_app.app_context():
         with pytest.raises(FileNotFoundError, match=expected):
             diag.open_diagnostic(diag.Variable.WIND, diag.MinimLoop.GUESS)
 
 
 @pytest.mark.skip(reason="Moto doesn't check authentication by default")
-def test_open_diagnostic_s3_unauthenticated(app, s3, tmp_path):
-    app.config["DIAG_DIR"] = f"s3://{test_bucket_name}/"
-    app.config["ENDPOINT_URL"] = endpoint_uri
-    app.config["LOCAL_CACHE_PATH"] = f"{tmp_path}/cache"
-
+def test_open_diagnostic_s3_unauthenticated(s3_app):
     os.unsetenv("AWS_ACCESS_KEY_ID")
     os.unsetenv("AWS_SECRET_ACCESS_KEY")
 
-    with app.app_context():
+    with s3_app.app_context():
         with pytest.raises(PermissionError):
             diag.open_diagnostic(diag.Variable.WIND, diag.MinimLoop.GUESS)
 
 
-def test_open_diagnostic_s3_unknown_backend(app, s3, tmp_path):
-    fsspec.filesystem("s3").clear_instance_cache()
-    S3FileSystem.clear_instance_cache()
-    diag_dir = Path(app.config["DIAG_DIR"].removeprefix("file://"))
+def test_open_diagnostic_s3_unknown_backend(s3_app, tmp_path):
     filename = "ncdiag_conv_uv_ges.nc4.2022050514"
-    test_file = diag_dir / filename
+    test_file = tmp_path / filename
     test_file.write_bytes(b"This is not a NetCDF4 file")
 
     # Upload to moto
@@ -215,15 +202,13 @@ def test_open_diagnostic_s3_unknown_backend(app, s3, tmp_path):
         Bucket=test_bucket_name, Filename=str(test_file), Key=f"pytest/{filename}"
     )
 
-    app.config["DIAG_DIR"] = f"s3://{test_bucket_name}/pytest/"
-    app.config["ENDPOINT_URL"] = endpoint_uri
-    app.config["LOCAL_CACHE_PATH"] = f"{tmp_path}/cache"
+    s3_app.config["DIAG_DIR"] = f"s3://{test_bucket_name}/pytest/"
 
     expected = (
         r"did not find a match in any of xarray's currently installed IO backends.*"
     )
 
-    with app.app_context():
+    with s3_app.app_context():
         with pytest.raises(ValueError, match=expected):
             diag.open_diagnostic(diag.Variable.WIND, diag.MinimLoop.GUESS)
 
@@ -250,27 +235,25 @@ def test_open_diagnostic_s3_unknown_backend(app, s3, tmp_path):
     ],
 )
 def test_open_diagnostic_s3(
-    variable, loop, filename, app, make_scalar_diag, s3, tmp_path
+    variable, loop, filename, s3_app, make_scalar_diag, tmp_path
 ):
     # Write the netcdf file to disk before uploading
     # Using a fileobj instead of writing to disk would be cleaner but requires using
     # the scipy engine
-    diag_dir = Path(app.config["DIAG_DIR"].removeprefix("file://"))
+    # diag_dir = Path(app.config["DIAG_DIR"].removeprefix("file://"))
     expected = make_scalar_diag(omf=[0, -1, 2])
-    expected.to_netcdf(diag_dir / filename)
+    expected.to_netcdf(tmp_path / filename)
 
     client = get_boto3_client()
     client.upload_file(
         Bucket=test_bucket_name,
-        Filename=str(diag_dir / filename),
+        Filename=str(tmp_path / filename),
         Key=f"pytest/{filename}",
     )
 
-    app.config["DIAG_DIR"] = f"s3://{test_bucket_name}/pytest/"
-    app.config["ENDPOINT_URL"] = endpoint_uri
-    app.config["LOCAL_CACHE_PATH"] = f"{tmp_path}/cache"
+    s3_app.config["DIAG_DIR"] = f"s3://{test_bucket_name}/pytest/"
 
-    with app.app_context():
+    with s3_app.app_context():
         result = diag.open_diagnostic(variable, loop)
 
     xr.testing.assert_equal(result, expected)
