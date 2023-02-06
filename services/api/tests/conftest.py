f00cbac95044920b610b928ed83cf3f6bbc69b80
W. Evan Sheehan <evan.sheehan@noaa.gov>
2023-02-06 14:45:39 -0700

    Copy diag data fixtures from data service branch

--- services/api/tests/conftest.py
+++ services/api/tests/conftest.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 
+import numpy as np
 import pytest
 import xarray as xr
 
@@ -28,6 +29,7 @@ def app(tmp_path):
 
     app = create_app()
     app.config["DIAG_DIR"] = str(diag_dir.as_uri())
+    app.config["DIAG_ZARR"] = tmp_path / "test_diag.zarr"
 
     yield app
 
@@ -48,3 +50,53 @@ def diag_file(app):
     files_created = []
 
     yield factory
+
+
+@pytest.fixture
+def diag_dataset():
+    def factory(variable: str, initialization_time: str, loop: str, **kwargs):
+        dims = [*kwargs.keys(), "nobs"]
+        shape = [*map(len, kwargs.values()), 2]
+        variables = [
+            "observation",
+            "forecast_adjusted",
+            "obs_minus_forecast_adjusted",
+            "forecast_unadjusted",
+            "obs_minus_forecast_unadjusted",
+        ]
+
+        ds = xr.Dataset(
+            {var: (dims, np.zeros(shape)) for var in variables},
+            coords=dict(
+                longitude=(["nobs"], np.array([90, -160], dtype=np.float64)),
+                latitude=(["nobs"], np.array([22, 25], dtype=np.float64)),
+                is_used=(["nobs"], np.array([1, 0], dtype=np.int8)),
+                **kwargs,
+            ),
+            attrs={
+                "name": variable,
+                "loop": loop,
+                "initialization_time": initialization_time,
+            },
+        )
+
+        return ds
+
+    return factory
+
+
+@pytest.fixture
+def diag_zarr(app, diag_dataset):
+    def factory(variables: list[str], initialization_time: str, loop: str):
+        with app.app_context():
+            zarr_file = app.config["DIAG_ZARR"]
+
+        for variable in variables:
+            coords = {"component": ["u", "v"]} if variable == "uv" else {}
+
+            ds = diag_dataset(variable, initialization_time, loop, **coords)
+            ds.to_zarr(zarr_file, group=f"/{variable}/{initialization_time}/{loop}")
+
+        return zarr_file
+
+    return factory
