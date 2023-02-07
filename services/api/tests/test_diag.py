bb537bc12c3e0a1ab5e8695f4a8452f0b7a3614b
W. Evan Sheehan <evan.sheehan@noaa.gov>
2023-02-07 13:36:54 -0700

    Remove superfluous tests and fixtures
    Remove the unrecognized backend tests as we are now explicitly using
    open_zarr instead of letting xarray guess the backend with
    open_diagnostic. Also remove the fixtures for the mocked AWS service and
    dependency on moto because our S3 tests run against AWS for real.

--- services/api/tests/test_diag.py
+++ services/api/tests/test_diag.py
@@ -1,109 +1,20 @@
 import os
-import time
 import uuid
-from pathlib import Path
 from unittest import mock
 
 import numpy as np
 import pytest
-import requests
 import xarray as xr
 
-from unified_graphics import create_app, diag
+from unified_graphics import diag
 
 # Global resources for s3
 test_bucket_name = "osti-modeling-dev-rtma-vis"
-port = 5555
-endpoint_uri = f"http://127.0.0.1:{port}/"
 
 
 @pytest.fixture
 def test_key_prefix():
     return f"/test/{uuid.uuid4()}/"
-
-
-# Fixture taken from fsspec/s3fs's test suite at
-# https://github.com/fsspec/s3fs/blob/e358d132757da3e47fafaea7420803eed102b19b/s3fs/tests/test_s3fs.py#L70-L110
-@pytest.fixture()
-def s3_base():
-    # writable local S3 system
-    import shlex
-    import subprocess
-
-    try:
-        # should fail since we didn't start server yet
-        r = requests.get(endpoint_uri)
-    except Exception:
-        pass
-    else:
-        if r.ok:
-            raise RuntimeError("moto server already up")
-    if "AWS_SECRET_ACCESS_KEY" not in os.environ:
-        os.environ["AWS_SECRET_ACCESS_KEY"] = "foo"
-    if "AWS_ACCESS_KEY_ID" not in os.environ:
-        os.environ["AWS_ACCESS_KEY_ID"] = "foo"
-    proc = subprocess.Popen(
-        shlex.split(f"moto_server s3 -p {port}"),
-        stderr=subprocess.DEVNULL,
-        stdout=subprocess.DEVNULL,
-        stdin=subprocess.DEVNULL,
-    )
-
-    timeout = 5
-    while timeout > 0:
-        try:
-            print("polling for moto server")
-            r = requests.get(endpoint_uri)
-        except Exception:
-            pass
-        else:
-            if r.ok:
-                break
-        timeout -= 0.1
-        time.sleep(0.1)
-    try:
-        print("polling done. testing if server is up")
-        r = requests.get(endpoint_uri)
-    except Exception as e:
-        raise RuntimeError("Moto server not up") from e
-    else:
-        if r.ok:
-            print("server up")
-    yield
-    print("moto done")
-    proc.terminate()
-    proc.wait()
-
-
-def get_boto3_client():
-    import boto3  # Use the boto3 included with moto since moto has strict dependencies
-
-    return boto3.client("s3", endpoint_url=endpoint_uri)
-
-
-@pytest.fixture()
-def s3_app(s3_base, tmp_path):
-    client = get_boto3_client()
-    client.create_bucket(Bucket=test_bucket_name, ACL="public-read")
-
-    app = create_app()
-    app.config["DIAG_DIR"] = f"s3://{test_bucket_name}/"
-    app.config["ENDPOINT_URL"] = endpoint_uri
-    app.config["LOCAL_CACHE_PATH"] = f"{tmp_path}/cache"
-
-    yield app
-
-
-@pytest.fixture
-def make_scalar_diag():
-    def _make_scalar_diag(omf):
-        return xr.Dataset(
-            {
-                "Obs_Minus_Forecast_unadjusted": omf,
-            }
-        )
-
-    return _make_scalar_diag
 
 
 @pytest.mark.parametrize(
@@ -156,38 +67,6 @@ def test_open_diagnostic(app, diag_dataset, diag_zarr):
     xr.testing.assert_equal(result, diag_dataset(str(variable), init_time, str(loop)))
 
 
-@pytest.mark.parametrize(
-    "variable,loop,filename",
-    [
-        (
-            diag.Variable.MOISTURE,
-            diag.MinimLoop.GUESS,
-            "ncdiag_conv_q_ges.nc4.2022050514",
-        ),
-        (
-            diag.Variable.PRESSURE,
-            diag.MinimLoop.ANALYSIS,
-            "ncdiag_conv_ps_anl.nc4.2022050514",
-        ),
-        (
-            diag.Variable.TEMPERATURE,
-            diag.MinimLoop.ANALYSIS,
-            "ncdiag_conv_t_anl.nc4.2022050514",
-        ),
-        (diag.Variable.WIND, diag.MinimLoop.GUESS, "ncdiag_conv_uv_ges.nc4.2022050514"),
-    ],
-)
-def test_open_diagnostic_local(variable, loop, filename, app, make_scalar_diag):
-    diag_dir = Path(app.config["DIAG_DIR"].removeprefix("file://"))
-    expected = make_scalar_diag(omf=[0, -1, 2])
-    expected.to_netcdf(diag_dir / filename)
-
-    with app.app_context():
-        result = diag.open_diagnostic(variable, loop)
-
-    xr.testing.assert_equal(result, expected)
-
-
 def test_open_diagnostic_local_does_not_exist(app):
     init_time = "2022-05-16T04:00"
     expected = r"No such file or directory: '.*test_diag.zarr'$"
@@ -195,20 +74,6 @@ def test_open_diagnostic_local_does_not_exist(app):
     with app.app_context():
         with pytest.raises(FileNotFoundError, match=expected):
             diag.open_diagnostic(diag.Variable.WIND, init_time, diag.MinimLoop.GUESS)
-
-
-def test_open_diagnostic_local_unknown_backend(app):
-    diag_dir = Path(app.config["DIAG_DIR"].removeprefix("file://"))
-    test_file = diag_dir / "ncdiag_conv_uv_ges.nc4.2022050514"
-    test_file.write_bytes(b"This is not a NetCDF4 file")
-
-    expected = (
-        r"did not find a match in any of xarray's currently installed IO backends.*"
-    )
-
-    with app.app_context():
-        with pytest.raises(ValueError, match=expected):
-            diag.open_diagnostic(diag.Variable.WIND, diag.MinimLoop.GUESS)
 
 
 @pytest.mark.parametrize(
@@ -274,28 +139,6 @@ def test_open_diagnostic_s3_unauthenticated(app, test_key_prefix):
 
     # Restore environment
     os.environ = prev_env
-
-
-def test_open_diagnostic_s3_unknown_backend(s3_app, tmp_path):
-    filename = "ncdiag_conv_uv_ges.nc4.2022050514"
-    test_file = tmp_path / filename
-    test_file.write_bytes(b"This is not a NetCDF4 file")
-
-    # Upload to moto
-    client = get_boto3_client()
-    client.upload_file(
-        Bucket=test_bucket_name, Filename=str(test_file), Key=f"pytest/{filename}"
-    )
-
-    s3_app.config["DIAG_DIR"] = f"s3://{test_bucket_name}/pytest/"
-
-    expected = (
-        r"did not find a match in any of xarray's currently installed IO backends.*"
-    )
-
-    with s3_app.app_context():
-        with pytest.raises(ValueError, match=expected):
-            diag.open_diagnostic(diag.Variable.WIND, diag.MinimLoop.GUESS)
 
 
 @pytest.mark.aws
