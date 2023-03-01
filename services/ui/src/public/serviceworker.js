654595fb90a43dad5c84ce74717a3db6fc611d5a
W. Evan Sheehan <evan.sheehan@noaa.gov>
2023-03-01 08:20:00 -0700

    Implement a simple service worker caching strategy
    Cache all the things! (For now.)

--- services/ui/src/public/serviceworker.js
+++ services/ui/src/public/serviceworker.js
@@ -22,3 +22,38 @@ self.addEventListener("install", () => {
   // Don't wait to activate the new service worker in open tabs.
   self.skipWaiting();
 });
+
+async function addToCache(key, value) {
+  const cache = await caches.open(VERSION);
+  await cache.put(key, value);
+}
+
+async function respondFromCache({ request, preloadResponse }) {
+  const cachedResponse = await caches.match(request);
+  if (cachedResponse) {
+    console.info(`[${request.url}] Response loaded from cache`);
+    return cachedResponse;
+  }
+
+  const preloadResponsePromise = await preloadResponse;
+  if (preloadResponse) {
+    console.info(`[${request.url}] Response preloaded`);
+    addToCache(request, preloadResponsePromise.clone());
+    return preloadResponse;
+  }
+
+  try {
+    const response = await fetch(request);
+    addToCache(request, response.clone());
+    return response;
+  } catch (error) {
+    console.error(`[Network error] ${error}`);
+    return new Response("Network error", {
+      status: 400,
+      header: { "Content-Type": "text/plain" },
+    });
+  }
+}
+self.addEventListener("fetch", (event) => {
+  event.respondWith(respondFromCache(event));
+});
