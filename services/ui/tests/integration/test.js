6e94bbf1a22b358a054988ef1656e932af20f9bf
W. Evan Sheehan <evan.sheehan@noaa.gov>
2023-03-06 08:17:46 -0700

    Remove the UI service

--- services/ui/tests/integration/test.js
+++
@@ -1,6 +0,0 @@
-import { expect, test } from "@playwright/test";
-
-test("application has expected title", async ({ page }) => {
-  await page.goto("/");
-  expect(await page.textContent("header")).toBe("Unified Graphics");
-});
