ce5985b89a4a134ca406ad0ac6f38d3f9a65826c
W. Evan Sheehan <evan.sheehan@noaa.gov>
2023-03-30 11:08:59 -0600

    Fix selection in the 2D histogram
    The code was still using the private xScale and yScale variables to
    invert the coordinates in the UI, but those aren’t being used by the
    actual drawing code, so they were just 1::1 with the screen coordinates.
    Those references to the private scale variables have been replaced with
    the getter for the scale.
    
    The format of the custom event’s detail has also been changed to be more
    useful to the event handler. The event handler has been updated to
    handle vector selections as well.

--- services/api/src/unified_graphics/static/js/diag.js
+++ services/api/src/unified_graphics/static/js/diag.js
@@ -2,12 +2,24 @@ function onChartBrush(event) {
   let { searchParams } = new URL(window.location);
 
   for (let [key, value] of Object.entries(event.detail)) {
-    if (!value) {
-      searchParams.delete(key);
-      continue;
-    }
+    // Clear out any existing params matching this key.
+    searchParams.delete(key);
 
-    searchParams.set(key, value.join("::"));
+    // If this key has no value, we're done
+    if (!value) continue;
+
+    if (Array.isArray(value[0])) {
+      // If the values are a mutlidimensional array, then we have a vector filter, and
+      // each element of the outer array is a range of values for one component of the
+      // vector, which needs to be added.
+      for (let component of value) {
+        searchParams.append(key, component.join("::"));
+      }
+    } else {
+      // If we're not dealing with a multidimensional array, then we can just join the
+      // components of this array.
+      searchParams.append(key, value.join("::"));
+    }
   }
 
   window.location.search = searchParams.toString();
