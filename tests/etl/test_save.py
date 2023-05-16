b444906cc9f4f6ffd226018511750785815b7779
W. Evan Sheehan <evan.sheehan@noaa.gov>
2023-05-16 13:32:34 -0600

    Test for db updates when adding new analyses

--- tests/etl/test_save.py
+++ tests/etl/test_save.py
@@ -1,13 +1,16 @@
 """Test saving xarray Datasets to Zarr"""
 
+from datetime import datetime
 from pathlib import Path
 
 import xarray as xr
+from sqlalchemy import func, select
 
 from unified_graphics.etl import diag
+from unified_graphics.models import Analysis, WeatherModel
 
 
-def test_save_new(tmp_path, diag_dataset):
+def test_save_new(tmp_path, diag_dataset, session):
     """diag.save should create a new Zarr on first write"""
 
     init_time = "2022-05-05T14"
@@ -63,6 +66,31 @@ def test_save_new(tmp_path, diag_dataset):
         ),
         ps,
     )
+
+    bg_model = session.scalar(
+        select(WeatherModel).where(WeatherModel.name == background)
+    )
+    assert bg_model, "Background model not created"
+
+    wx_model = session.scalar(
+        select(WeatherModel).where(
+            WeatherModel.name == model, WeatherModel.background_id == bg_model.id
+        )
+    )
+    assert wx_model, "Model not created in database"
+
+    analysis_count = session.scalar(
+        select(func.count())
+        .select_from(Analysis)
+        .where(
+            Analysis.time == datetime.fromisoformat(init_time),
+            Analysis.domain == domain,
+            Analysis.frequency == frequency,
+            Analysis.system == system,
+            Analysis.model_id == wx_model.id,
+        )
+    )
+    assert analysis_count == 1, "Analysis row not created in database"
 
 
 def test_add_loop(diag_zarr, diag_dataset):
