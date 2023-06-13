13eebab568c5de41954889ad868ff03489325210
W. Evan Sheehan <evan.sheehan@noaa.gov>
2023-06-13 14:09:12 -0600

    Add tests for writing the parquet file during etl
    Add some assertions to check that the parquet file is being created
    during ETL. This is getting to be a bit much with the Zarr and database
    stuff as well. If this works out, we should consider using just the
    Parquet files for diag data and removing Zarr and Postgres from the
    equation.

--- tests/etl/test_save.py
+++ tests/etl/test_save.py
@@ -2,6 +2,7 @@
 
 from datetime import datetime
 
+import pandas as pd
 import pytest
 import xarray as xr
 from sqlalchemy import func, select
@@ -63,9 +64,10 @@ def t_anl(analysis, diag_dataset, session, zarr_file):
     return t
 
 
-def test_save_new(ps_anl, analysis, zarr_file, session):
+def test_save_new(ps_anl, analysis, zarr_file, tmp_path, session):
     """diag.save should create a new Zarr on first write"""
     (init_time, model, system, domain, frequency, background) = analysis
+    parquet_file = tmp_path / "_".join((model, background, system, domain, frequency))
 
     xr.testing.assert_equal(
         xr.open_zarr(
@@ -77,6 +79,21 @@ def test_save_new(ps_anl, analysis, zarr_file, session):
             consolidated=False,
         ),
         ps_anl,
+    )
+
+    expected_df = ps_anl.to_dataframe()
+    expected_df["observation_class"] = "ps"
+    expected_df["loop"] = "anl"
+    expected_df = expected_df.astype(
+        {"observation_class": "category", "loop": "category"}
+    )
+
+    pd.testing.assert_frame_equal(
+        pd.read_parquet(
+            parquet_file,
+            filters=(("observation_class", "=", "ps"), ("loop", "=", "anl")),
+        ),
+        expected_df,
     )
 
     bg_model = session.scalar(
@@ -105,10 +122,11 @@ def test_save_new(ps_anl, analysis, zarr_file, session):
     assert analysis_count == 1, "Analysis row not created in database"
 
 
-def test_add_loop(ps_anl, ps_ges, analysis, zarr_file, session):
+def test_add_loop(ps_anl, ps_ges, analysis, zarr_file, tmp_path, session):
     """diag.save should add a new group for each loop"""
 
     (init_time, model, system, domain, frequency, background) = analysis
+    parquet_file = tmp_path / "_".join((model, background, system, domain, frequency))
 
     xr.testing.assert_equal(
         xr.open_zarr(
@@ -134,14 +152,44 @@ def test_add_loop(ps_anl, ps_ges, analysis, zarr_file, session):
         ps_ges,
     )
 
+    loop_class = pd.CategoricalDtype(categories=["anl", "ges"], ordered=False)
+    expected_df = ps_anl.to_dataframe()
+    expected_df["observation_class"] = "ps"
+    expected_df["loop"] = "anl"
+    expected_df = expected_df.astype(
+        {"observation_class": "category", "loop": loop_class}
+    )
+    pd.testing.assert_frame_equal(
+        pd.read_parquet(
+            parquet_file,
+            filters=(("observation_class", "=", "ps"), ("loop", "=", "anl")),
+        ),
+        expected_df,
+    )
+
+    expected_df = ps_ges.to_dataframe()
+    expected_df["observation_class"] = "ps"
+    expected_df["loop"] = "ges"
+    expected_df = expected_df.astype(
+        {"observation_class": "category", "loop": loop_class}
+    )
+    pd.testing.assert_frame_equal(
+        pd.read_parquet(
+            parquet_file,
+            filters=(("observation_class", "=", "ps"), ("loop", "=", "ges")),
+        ),
+        expected_df,
+    )
+
     analysis_count = session.scalar(select(func.count()).select_from(Analysis))
     assert analysis_count == 1
 
 
-def test_add_variable(ps_anl, t_anl, analysis, zarr_file, session):
+def test_add_variable(ps_anl, t_anl, analysis, zarr_file, tmp_path, session):
     """diag.save should insert new variables into existing groups"""
 
     (init_time, model, system, domain, frequency, background) = analysis
+    parquet_file = tmp_path / "_".join((model, background, system, domain, frequency))
 
     xr.testing.assert_equal(
         xr.open_zarr(
@@ -165,6 +213,35 @@ def test_add_variable(ps_anl, t_anl, analysis, zarr_file, session):
             consolidated=False,
         ),
         t_anl,
+    )
+
+    obs_class = pd.CategoricalDtype(categories=["ps", "t"], ordered=False)
+    expected_df = ps_anl.to_dataframe()
+    expected_df["observation_class"] = "ps"
+    expected_df["loop"] = "anl"
+    expected_df = expected_df.astype(
+        {"observation_class": obs_class, "loop": "category"}
+    )
+    pd.testing.assert_frame_equal(
+        pd.read_parquet(
+            parquet_file,
+            filters=(("observation_class", "=", "ps"), ("loop", "=", "anl")),
+        ),
+        expected_df,
+    )
+
+    expected_df = t_anl.to_dataframe()
+    expected_df["observation_class"] = "t"
+    expected_df["loop"] = "anl"
+    expected_df = expected_df.astype(
+        {"observation_class": obs_class, "loop": "category"}
+    )
+    pd.testing.assert_frame_equal(
+        pd.read_parquet(
+            parquet_file,
+            filters=(("observation_class", "=", "t"), ("loop", "=", "anl")),
+        ),
+        expected_df,
     )
 
     analysis_count = session.scalar(select(func.count()).select_from(Analysis))
