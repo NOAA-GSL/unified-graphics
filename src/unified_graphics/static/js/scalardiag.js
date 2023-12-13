c69d028bb6923ebb1a85c4f501ad8dbcbc696b50
W. Evan Sheehan <evan.sheehan@noaa.gov>
2023-12-13 11:45:02 -0700

    Rename ChartTimeSeries to ChartOMBHistory
    Since this chart is being treated as a representation of a specific set
    of variables, it makes sense to give it a more specific name. It is not
    a general-purpose time series.

--- src/unified_graphics/static/js/scalardiag.js
+++ src/unified_graphics/static/js/scalardiag.js
@@ -3,5 +3,5 @@ import ChartContainer from "./component/ChartContainer.js";
 import ChartHistogram from "./component/ChartHistogram.js";
 import ChartMap from "./component/ChartMap.js";
 import ChartObsCount from "./component/ChartObsCount.js";
-import ChartTimeseries from "./component/ChartTimeSeries.js";
+import ChartOMBHistory from "./component/ChartOMBHistory.js";
 import ColorRamp from "./component/ColorRamp.js";
